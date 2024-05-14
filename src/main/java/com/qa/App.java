a72b8768e061881abfb39b3577bb4d515144b469
Michael Sweeney <Michael.b.sweeney96@gmail.com>
2024-05-14 16:16:46 +0100

    add hello victoria

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -15,6 +15,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Lesley"));
         System.out.println(sayHelloToSomeone("Michael"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }

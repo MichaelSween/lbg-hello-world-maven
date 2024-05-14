071567fb920e601561fc4bb457d520f2ac9bcc2c
Michael Sweeney <Michael.b.sweeney96@gmail.com>
2024-05-14 12:29:18 +0100

    Added lesley to the list

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Lesley"));
         System.out.println(sayHelloToSomeone("Michael"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
